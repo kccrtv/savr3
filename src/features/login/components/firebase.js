e48c8184e2b966da30730888defcead9ce85e5ae
kccrtv <kimcincocreative@gmail.com>
2021-04-25 17:35:52 -0700

    update env vars and firebase initialization

--- src/features/login/components/firebase.js
+++ src/features/login/components/firebase.js
@@ -11,6 +11,7 @@ const app = firebase.initializeApp({
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 export const auth = app.auth();
